905f43727f962b80774fcdb6f52559c367aac863
Quintin <quintin.balsdon@zuhlke.com>
2020-12-27 10:46:31 +0000

    adding new scripts and enabling formatting of the XML output

--- templates/script.sh
+++ templates/script.sh
@@ -2,16 +2,25 @@
 
 cleanup () {
     echo "~~ CLEAN UP ~~"
-} 
+}
 trap cleanup EXIT
 
 EXEC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+USAGE=""
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -d|--deploy) deploy="$2"; shift ;;
+        -s|--device) DEVICE="$2"; shift ;;
         -u|--uglify) uglify=1 ;;
+        -h|--help) echo $USAGE; exit 0;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
+
+if [ -z "$DEVICE" ]; then
+  DEVICE=${adb devices | sed -n 2p | awk '{print $1;}'}
+fi
+
+if [ -z "$DEVICE" ]; then
+  echo "No devices or emulators attached"
+fi
