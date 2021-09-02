fd71387c261752336732326feba9d34443c193d6
Quintin Balsdon <quba@192.168.1.200>
2021-09-02 16:57:38 +0100

    dummy server script

--- python/tests/server_test.sh
+++ python/tests/server_test.sh
@@ -1,26 +1,3 @@
 #!/bin/sh
 
-check_arr=()
-for filename in *; do
-  if [[ "${filename}" =~ "_test.py" ]]; then
-    echo "TESTING ${filename}"
-    ./${filename}
-    result=$?
-    if [[ "${result}" == 1 ]]; then
-      check_arr+=("${filename}")
-    fi
-    echo "--------------------------------------------------"
-  fi
-done
-
-if [[ "${#check_arr[@]}" == 0 ]]; then
-  echo "ALL TESTS PASSING"
-  exit 0
-else
-  echo "~~~~~~~~~~~~~~~~~~~~~~~FAILURES~~~~~~~~~~~~~~~~~~~~~~~"
-  for filename in ${check_arr}; do
-    echo "    ${filename}"
-  done
-  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  exit 1
-fi
+echo "Hello server"
